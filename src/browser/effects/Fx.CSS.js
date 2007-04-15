c8266a3ae2b180f000ea0d731494b39d654e9b2a
Jürg Lehni <juerg@scratchdisk.com>
2007-04-15 22:32:55 +0000

    Removed headers mentioning Mootools.net, as there is already one in Fx.js
    git-svn-id: https://svn.scratchdisk.com/bootstrap/trunk@6 917dd4d6-6a9c-4700-b622-114467236220

--- src/browser/effects/Fx.CSS.js
+++ src/browser/effects/Fx.CSS.js
@@ -1,13 +1,6 @@
 #ifndef __browser_Fx_CSS__
 #define __browser_Fx_CSS__
 
-/**
- * Some code in this file is based on Mootools.net and adapted to the
- * architecture of Bootstrap, with added changes in design and architecture
- * where deemeded necessary.
- * See http://www.bootstrap-js.net/wiki/MootoolsDifferences
- */
-
 #include "Fx.js"
 
 ////////////////////////////////////////////////////////////////////////////////
