14b95f8195ace21d92107a03c539928ff1f6fe4d
Jürg Lehni <juerg@scratchdisk.com>
2007-04-19 19:09:52 +0000

    Removed unused stuff.
    git-svn-id: https://svn.scratchdisk.com/bootstrap/trunk@15 917dd4d6-6a9c-4700-b622-114467236220

--- src/lang/Number.js
+++ src/lang/Number.js
@@ -6,22 +6,10 @@
 
 #include "String.js"
 
-// an example how other prototypes could be improved.
 Number.inject({
 	// tell $typeof that number objects are numbers too.
 	_type: 'number',
-
-	/**
-	 * Executes the passed iterator as many times as specified by the
-	 * numerical value.
-	 */
-	times: function(iter) {
-		for (var i = 0; i < this; i++) iter();
-		return this;
-	},
-
 	toInt: String.prototype.toInt,
-
 	toFloat: String.prototype.toFloat
 });
 
