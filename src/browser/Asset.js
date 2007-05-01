d598615461f1b3b9a50b06f0180b18e8ac30cfc0
Jürg Lehni <juerg@scratchdisk.com>
2007-05-01 13:10:45 +0000

    Synchronized with latest Mootools, fixing memory leaks.
    git-svn-id: https://svn.scratchdisk.com/bootstrap/trunk@32 917dd4d6-6a9c-4700-b622-114467236220

--- src/browser/Asset.js
+++ src/browser/Asset.js
@@ -15,50 +15,51 @@
 
 Asset = {
 	javascript: function(src, props) {
-		return Asset.create('script', {
-			type: 'text/javascript', src: src
-		}, props, true);
+		var script = new Element('script', { src: src }).addEvents({
+			load: props.onLoad, // can be null
+			readystatechange: function(){
+				if (this.readyState == 'complete') this.fireEvent('load');
+			}
+		});
+		delete props.onload;
+		return script.setProperties(props).inject(document.head);
 	},
 
 	css: function(src, props) {
-		return Asset.create('link', {
+		return new Element('link', $H({
 			rel: 'stylesheet', media: 'screen', type: 'text/css', href: src
-		}, props, true);
+		}).merge(props)).inject(document.head);
 	},
 
 	image: function(src, props) {
 		props = props || {};
-		var img = new Image();
-		var onLoad = props.onLoad, done = false;
-		img.onload = props.onLoad = function() {
-			if (onLoad && !done) {
-				done = true;
-				return onLoad();
-			}
-		}
-		// BROWSER: Set src after onLoad, as on offline Opera, it loads
-		// the image immediatelly!
-		img.src = props.src = src;
-		return Asset.create('img', props);
+		var image = new Image();
+		image.src = src;
+		var element = new Element('img', { src: src });
+		['load', 'abort', 'error'].each(function(type) {
+			var event = props['on' + type];
+			delete props['on' + type];
+			if (event) element.addEvent(type, function(){
+				this.removeEvent(type, arguments.callee);
+				event.call(this);
+			});
+		});
+		if (image.width && image.height) element.fireEvent.delay(1, element, 'load');
+		return element.setProperties(props);
 	},
 
  	images: function(srcs, opts) {
 		opts = opts || {};
 		if ($typeof(srcs) != 'array') srcs = [srcs];
 		var imgs = [], count = 0;
-		return srcs.each(function(src) {
+		return $$(srcs.each(function(src) {
 			this.push(Asset.image(src, {
 				onLoad: function() {
 					if (opts.onProgress) opts.onProgress(src);
 					if (++count == srcs.length && opts.onComplete) opts.onComplete();
 				}
 			}));
-		}, []);
-	},
-
-	create: function(type, defs, props, inject) {
-		var el = new Element(type).setProperties($H(defs).merge(props));
-		return inject ? el.injectInside($E('head')) : el;
+		}, []));
 	}
 };
 
