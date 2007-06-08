dd5b697a8fd0c03de4dfb3ed361e4c151212d011
Jürg Lehni <juerg@scratchdisk.com>
2007-06-08 14:52:03 +0000

    Added two new files left out in the previous commit.
    git-svn-id: https://svn.scratchdisk.com/bootstrap/trunk@59 917dd4d6-6a9c-4700-b622-114467236220

---
+++ src/browser/Document.js
@@ -0,0 +1,11 @@
+#ifndef __browser_Document__
+#define __browser_Document__
+
+////////////////////////////////////////////////////////////////////////////////
+// Document
+
+Document = {
+	$: document // Used for the Event methods
+};
+
+#endif // __browser_Document__
