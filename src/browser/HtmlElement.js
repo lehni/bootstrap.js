14ad197a823070335444055cd47df44454abe539
Jürg Lehni <juerg@scratchdisk.com>
2007-06-10 19:00:01 +0000

    Renamed Element -> HtmlElement. Added Math.js.
    git-svn-id: https://svn.scratchdisk.com/bootstrap/trunk@61 917dd4d6-6a9c-4700-b622-114467236220

--- src/browser/HtmlElement.js
+++ src/browser/HtmlElement.js
@@ -1,5 +1,5 @@
-#ifndef __browser_Element__
-#define __browser_Element__
+#ifndef __browser_HtmlElement__
+#define __browser_HtmlElement__
 
 #ifdef HIDDEN
 /**
@@ -11,43 +11,59 @@
 #endif
 
 ////////////////////////////////////////////////////////////////////////////////
-// Elements
+// HtmlElements
 
 /**
- * Elements extends inject so that each of the functions
+ * HtmlElements extends inject so that each of the functions
  * defined bellow are executed on the whole set of elements, and the
- * returned values are collected in an array and converted to a Elements
+ * returned values are collected in an array and converted to a HtmlElements
  * array again, if it contains only html elements.
  */
-Elements = Base.extend({
-	$constructor: function(els, convert) {
-		// [].inject(this.__proto__) creates the enriched collection array.
-		// TODO: Transition
-		return (convert && els ? els : $A(els)).inject(this.__proto__);
+HtmlElements = Array.extend({
+	initialize: function(els) {
+		// Instances of HtmlElements are not real arrays, but they inherit from
+		// Array. Setting length to 0 is all that's needed to make all the
+		// native array functions works. Modifying length later will not clear
+		// the fields from the array though.
+		this.length = 0;
+		// Do not use els.push do detect arrays, as we're passing pseudo arrays
+		// here too (e.g. childNodes). But Option defines .length too, so rule
+		// that out by checking nodeType as well.
+		this.append(els && els.length && !els.nodeType ? els : arguments);
 	},
 
-	$static: {
+	/**
+	 * Only push wraps the added element in a HtmlElement. splice and unshift
+	 * are not overridden to do the same.
+	 */
+	push: function(el) {
+		return this.base(HtmlElement.get(el));
+	},
+
+	statics: {
 		inject: function(src) {
-			// For reach function that is injected into Elements, create a new
-			// function that iterates that calls the function on each of the
+			src = typeof src == 'function' ? new src() : src || [];
+			// For each function that is injected into HtmlElements, create a
+			// new function that iterates that calls the function on each of the
 			// collection's elements.
 			// src can either be a function to be called, or a object literal.
-			return this.$super(EACH((typeof src == 'function' ? src() : src), function(val, key) {
+			return this.base(EACH(src, function(val, key) {
 				this[key] = typeof val != 'function' ? val : function() {
 					// Only collect values if calling a getter function, otherwise
 					// return this
-					var args = arguments, values = /^get/.test(key) && [], els = true;
+					var args = arguments, get = /^get/.test(key), values;
 					this.each(function(obj) {
 						// Try to use original method if it's there, in order
-						// to support $super, as this will be the wrapper that
+						// to support base, as this will be the wrapper that
 						// sets it
 						var ret = (obj[key] || val).apply(obj, args);
-						if (values) {
+						if (get) {
+							values = values || ($typeof(ret) == 'element')
+								? new HtmlElements() : [];
 							values.push(ret);
-							els = els && $typeof(ret) == 'element';
 						}
 					});
-					return values ? (els ? new Elements(values, true) : values) : this;
+					return values || this;
 				}
 			}, {}));
 		}
@@ -55,19 +71,59 @@ Elements = Base.extend({
 });
 
 ////////////////////////////////////////////////////////////////////////////////
-// Element
+// HtmlElement
 
-Element = Base.extend(function() {
+HtmlElement = Base.extend(function() {
 	var cache = {}, tags = {}, uniqueId = 0;
 
+	// Garbage collection - uncache elements/purge listeners on orphaned elements
+	// so we don't hold a reference and cause the browser to retain them
+	(function() {
+		cache.each(function(obj, id) {
+			var el = obj.$;
+	        if(!el || !el.parentNode || (!el.offsetParent && !document.getElementById(id))) {
+	            if(el && obj.dispose) obj.dispose();
+	            delete cache[id];
+	        }
+		});
+	}).periodic(30000);
+
+	// Private inject function for HtmlElement. It adds support for
+	// _methods and _properties declarations, which forward calls and define
+	// getter / setters for fields of the native DOM node.
+	function inject(src, base) {
+		src = typeof src == 'function' ? new src() : src || []; 
+		// Forward method calls. Returns result if any, otherwise reference
+		// to this.
+		(src._methods || []).each(function(name) {
+			src[name] = function() {
+				var ret = this.$[name].apply(this.$, arguments);
+				return ret === undefined ? this : ret;
+			}
+		});
+		// Define getter / setters
+		(src._properties || []).each(function(name) {
+			var part = name.capitalize();
+			src['get' + part] = function() {
+				return this.$[name];
+			}
+			src['set' + part] = function(value) {
+				this.$[name] = value;
+			}
+		});
+		delete src._methods;
+		delete src._properties;
+		return Function.inject.call(this, src, base);
+	}
+
 	return {
-		$constructor: function(el, props) {
-			// Support element creating constructors on subclasses of Element
-			// that define prototype.tag and can take one argument, which 
+		initialize: function(el, props) {
+			// Support element creating constructors on subclasses of HtmlElement
+			// that define prototype._tag and can take one argument, which 
 			// defines the properties to be set:
-			if (this.tag && /^(object|hash)$/.test($typeof(el))) {
+			if (this._tag && /^(object|hash)$/.test($typeof(el))) {
 				props = el;
-				el = this.tag;
+				el = this._tag;
 			}
 			if (typeof(el) == 'string') {
 				if (Browser.IE && props && (props.name || props.type))
@@ -77,10 +133,10 @@ Element = Base.extend(function() {
 			}
 			var id = el.id;
 			// Cache wrappers by their ids
-			if(id && cache[id]) return cache[id]; // Element object already exists
+			if(id && cache[id]) return cache[id]; // HtmlElement object already exists
 			// Make sure we're using the right constructor for this tag
-			var ctor = tags[el.tagName.toLowerCase()] || Element;
-			if (!(this instanceof ctor)) return new ctor(el, props);
+			var ctor = tags[el.tagName.toLowerCase()] || HtmlElement;
+			if (this.constructor != ctor) return new ctor(el, props);
 			this.$ = el;
 			// Generate ids if the element does not define one
 			if (!id) id = el.id = 'bootstrap-' + (++uniqueId);
@@ -91,70 +147,65 @@ Element = Base.extend(function() {
 			cache[id] = this;
 		},
 
-		$static: {
-			inject: function(src, hide, base) {
-				// Inject not only into this, but also into the Elements collection
-				// where the functions are "multiplied" for each of the elements
-				// of the collection.
-				Elements.inject(src, hide, base);
-				return this.$super(src, hide, base);
+		statics: {
+			inject: function(src, base) {
+				var ret = inject.call(this, src, base);
+				// Now, after src was processed in #inject, inject not only into
+				// this, but also into HtmlElements where the functions are
+				// "multiplied" for each of the elements of the collection.
+				HtmlElements.inject(src, base);
+				return ret;
 			},
 
-			extend: function(src, hide) {
-				// Do not pass src to $super, as we weed to fix #inject first.
-				var ret = this.$super({}, hide);
-				// Undo overriding of the inject above for subclasses, as only
-				// injecting into Element (not subclasses) shall also inject
-				// into Elements!
-				ret.inject = Function.inject;
-				ret.inject(src, hide, this);
-				// When extending Element with a tag field specified, this 
+			extend: function(src) {
+				// Do not pass src to base, as we weed to fix #inject first.
+				var ret = this.base();
+				// Undo overriding of the inject method above for subclasses,
+				// as only injecting into HtmlElement (not subclasses) shall also
+				// inject into HtmlElements!
+				inject.call(ret, src, this);
+				// Now reset inject. Reseting before does not work, as it would
+				// be overridden during static inheritance again.
+				ret.inject = inject;
+				// When extending HtmlElement with a tag field specified, this 
 				// prototype will be used when wrapping elements of that type.
 				// If this is a prototype for a certain tag, store it in the LUT.
-				if (src.tag) tags[src.tag] = ret;
+				if (src && src._tag) tags[src._tag] = ret;
 				return ret;
 			},
 
 			get: function(el, root) {
 				if (typeof(el) == 'string') {
-					root = root && Element.unwrap(root);
+					root = HtmlElement.unwrap(root);
 					if (el.charAt(0) == '#') {
 						el = document.getElementById(el.substring(1));
-						if (root && !Element.isAncestor(el, root)) el = null;
+						if (el && root && !DomQuery.isAncestor(el, root))
+							el = null;
 					} else {
-						el = Element.select(el, root, 1)[0];
+						el = HtmlElement.select(el, root, 1)[0];
 					}
 				}
-				return el ? el.id && cache[el.id] || new Element(el) : null;
+				return el ? el.id && cache[el.id] || new HtmlElement(el) : null;
 			},
 
 			unwrap: function(el) {
-				return el.nodeType ? el : el.$;
-			},
-
-			// TODO: move further down?
-			isAncestor: function(el, parent) {
-				// TODO: See Ext for a faster implementation
-				parent = Element.unwrap(parent);
-				if (parent != document)
-					for (el = Element.unwrap(el).parentNode; el != parent; el = el.parentNode)
-						if (!el) return false;
-				return true;
+				return !el || el.nodeType ? el : el.$;
 			}
 		}
 	}
 });
 
-// Short-cut to Element.get
-$ = Element.get;
+// Short-cut to HtmlElement.get
+$ = HtmlElement.get;
 
-// Use the modified inject function from above which injects both into Element
-// and Elements.
-Element.inject(function() {
+// Use the modified inject function from above which injects both into HtmlElement
+// and HtmlElements.
+HtmlElement.inject(function() {
+	
 	function walk(el, name, start) {
 		el = el[start ? start : name];
 		while (el && $typeof(el) != 'element') el = el[name];
-		return Element.get(el);
+		return HtmlElement.get(el);
 	}
 
 	// Html to JS property mappings, as used by getProperty, setProperty
@@ -163,7 +214,8 @@ Element.inject(function() {
 		'class': 'className', 'for': 'htmlFor', colspan: 'colSpan',
 		rowspan: 'rowSpan', accesskey: 'accessKey', tabindex: 'tabIndex',
 		maxlength: 'maxLength', readonly: 'readOnly', value: 'value',
-		disabled: 'disabled', checked: 'checked', multiple: 'multiple'
+		disabled: 'disabled', checked: 'checked', multiple: 'multiple',
+		selected: 'selected'
 	};
 
 	var setterCache = {};
@@ -188,11 +240,11 @@ Element.inject(function() {
 		},
 
 		getElements: function(selectors) {
-			return Element.select(selectors || '*', this);
+			return HtmlElement.select(selectors || '*', this);
 		},
 
 		getElement: function(selector) {
-			return Element.get(selector || '*', this);
+			return HtmlElement.get(selector || '*', this);
 		},
 
 		getParents: function(selector) {
@@ -200,12 +252,12 @@ Element.inject(function() {
 			for (var el = this.$.parentNode; el; el = el.parentNode)
 				parents.push(el);
 			return selector && selector != '*'
-				? Element.filter(parents, selector, this)
+				? HtmlElement.filter(parents, selector, this)
 				: parents;
 		},
 
 		appendChild: function(el) {
-			this.$.appendChild(Element.get(el).$);
+			this.$.appendChild(HtmlElement.get(el).$);
 			return this;
 		},
 
@@ -221,13 +273,13 @@ Element.inject(function() {
 		},
 
 		insertBefore: function(el) {
-			el = Element.get(el);
+			el = HtmlElement.get(el);
 			el.$.parentNode.insertBefore(this.$, el.$);
 			return this;
 		},
 
 		insertAfter: function(el) {
-			el = Element.get(el);
+			el = HtmlElement.get(el);
 			var next = el.getNext();
 			if (!next) el.$.parentNode.appendChild(this.$);
 			else el.$.parentNode.insertBefore(this.$, next.$);
@@ -237,13 +289,13 @@ Element.inject(function() {
 		insertFirst: function(el) {
 			// TODO: See Ext's insertFirst, add support for createChild, return
 			// added element instead of this?!
-			el = Element.get(el);
+			el = HtmlElement.get(el);
 			el.$.insertBefore(this.$, el.$.firstChild);
 			return this;
 		},
 
 		insertInside: function(el) {
-			Element.get(el).appendChild(this);
+			HtmlElement.get(el).appendChild(this);
 			return this;
 		},
 
@@ -252,12 +304,18 @@ Element.inject(function() {
 			return this;
 		},
 
+		removeChild: function(el) {
+			el = HtmlElement.get(el);
+			this.$.removeChild(el.$);
+			return el;
+		},
+
 		clone: function(contents) {
-			return Element.get(this.$.cloneNode(!!contents));
+			return HtmlElement.get(this.$.cloneNode(!!contents));
 		},
 
 		replaceWith: function(el) {
-			el = Element.get(el);
+			el = HtmlElement.get(el);
 			this.$.parentNode.replaceChild(el.$, this.$);
 			return el;
 		},
@@ -304,19 +362,23 @@ Element.inject(function() {
 		},
 
 		getParent: function() {
-			return Element.get(this.$.parentNode);
+			return HtmlElement.get(this.$.parentNode);
 		},
 
 		getChildren: function() {
-			return new Elements(this.$.childNodes);
+			return new HtmlElements(this.$.childNodes);
+		},
+
+		removeChildren: function() {
+			this.getChildren().remove();
 		},
 
 		hasParent: function(el) {
-			return Element.isAncestor(this.$, el);
+			return DomQuery.isAncestor(this.$, HtmlElement.unwrap(el));
 		},
 
 		hasChild: function(el) {
-			return Element.isAncestor(el, this.$);
+			return DomQuery.isAncestor(HtmlElement.unwrap(el), this.$);
 		},
 
 		getTag: function() {
@@ -325,7 +387,7 @@ Element.inject(function() {
 
 		getProperty: function(name) {
 			var key = properties[name];
-			return (key) ? this.$[key] : this.$.getAttribute(name);
+			return key ? this.$[key] : this.$.getAttribute(name);
 		},
 
 		removeProperty: function(name) {
@@ -344,7 +406,7 @@ Element.inject(function() {
 
 		setProperties: function(src) {
 			return EACH(src, function(val, name) {
-				this.$.setAttribute(name, val);
+				this.setProperty(name, val);
 			}, this);
 		},
 
@@ -359,108 +421,4 @@ Element.inject(function() {
 	}
 });
 
-Form = Element.extend({
-	tag: 'form',
-
-	/**
-	 * Overrides getElements to only return form elements by default.
-	 * Explicitely call with '*' to return all elements contained in this form.
-	 */
-	getElements: function(selectors) {
-		return this.$super(selectors || ['input', 'select', 'textarea']);
-	},
-
-	blur: function() {
-		this.getElements().each(function(el) {
-			el.blur();
-		});
-		return this;
-	},
-
-	enable: function(enable) {
-		this.getElements().each(function(el) {
-			el.enable(enable);
-		});
-		return this;
-	}
-});
-
-FormElement = Element.extend({
-	enable: function(enable) {
-		var disable = !enable && enable !== undefined;
-		if (disable) this.$.blur();
-		this.$.disabled = disable;
-	},
-
-	focus: function() {
-		this.$.focus();
-		return this;
-	},
-
-	blur: function() {
-		this.$.blur();
-		return this;
-	}
-});
-
-Input = FormElement.extend({
-	tag: 'input',
-
-	getValue: function() {
-		if (this.$.checked && /checkbox|radio/.test(this.$.type) ||
-			/^(hidden|text|password)$/.test(this.$.type))
-			return this.$.value;
-	}
-});
-
-Select = FormElement.extend({
-	tag: 'select',
-
-	/**
-	 * Form Element constructors work in two ways:
-	 * - Thew wrap an existing element, in which facse first is the elment
-	 *   and second point to the properties to set.
-	 * - They create a new element, in which case first point to the properties to set
-	 *   and second might be something else, e.g. an array of options for Select.
-	 */
-	$constructor: function(first, second) {
-		var wrapping = $typeof(first) == 'element';
-		this.$super(wrapping ? first : second, wrapping ? second : null);
-		if (second && !wrapping) this.add.apply(this, second);
-		// TODO: redundant? Make native options available just like this.$.style
-		this.options = this.$.options;
-	},
-
-	getValue: function() {
-		if (this.$.selectedIndex != -1)
-			return this.$.options[this.$.selectedIndex].value;
-	},
-
-	getOptions: function() {
-		return this.$.options;
-	},
-
-	add: function() {
-		EACH(arguments, function(opt) {
-			this.$.options[this.$.options.length] = $typeof(opt) == 'object' ? opt
-					: new Option(opt);
-		}, this);
-		return this;
-	},
-
-	clear: function() {
-		this.$.selectedIndex = -1;
-		this.$.options.length = 0;
-		return this;
-	}
-});
-
-TextArea = FormElement.extend({
-	tag: 'textarea',
-
-	getValue: function() {
-		return this.$.value;
-	}
-});
-
-#endif // __browser_Element__
+#endif // __browser_HtmlElement__
---
+++ src/lang/Math.js
@@ -0,0 +1,13 @@
+#ifndef __lang_Math__
+#define __lang_Math__
+
+////////////////////////////////////////////////////////////////////////////////
+// Math
+
+Math.inject({
+	rand: function(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
+})
+
+#endif // __lang_Math__
