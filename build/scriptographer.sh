86ccd4662b72e821807bfb4ccbcf8452df711697
Jürg Lehni <juerg@scratchdisk.com>
2008-11-06 21:37:14 +0000

    Added build scripts for Scriptographer, boots and slickspeed.
    git-svn-id: https://svn.scratchdisk.com/bootstrap/trunk@282 917dd4d6-6a9c-4700-b622-114467236220

---
+++ build/scriptographer.sh
@@ -0,0 +1,9 @@
+./build.sh stripped
+
+# scriptographer
+cp ../out/bootstrap-scriptographer.js ~/Development/Scriptographer/Scriptographer/scripts/libraries/00_bootstrap.js
+# cp ../out/Template-rhino.js ~/Development/Scriptographer/Scriptographer/scripts/libraries/Template.js
+
+# doclet
+cp ../out/Template-rhino.js ~/Development/Scriptographer/Scriptographer/build/java/doclet/template.js
+cp ../out/bootstrap-rhino.js ~/Development/Scriptographer/Scriptographer/build/java/doclet/bootstrap.js
