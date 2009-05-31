984242dc0d6bdba44e7d8b548b3849532754aa5e
Jürg Lehni <juerg@scratchdisk.com>
2009-05-31 15:30:26 +0000

    Updated scriptographer folder locations.
    git-svn-id: https://svn.scratchdisk.com/bootstrap/trunk@316 917dd4d6-6a9c-4700-b622-114467236220

--- build/scriptographer.sh
+++ build/scriptographer.sh
@@ -1,9 +1,9 @@
 ./build.sh stripped
 
 # scriptographer
-cp ../out/bootstrap-scriptographer.js ~/Development/Scriptographer/Scriptographer/src/js/lib/00_bootstrap.js
-# cp ../out/Template-rhino.js ~/Development/Scriptographer/Scriptographer/scripts/libraries/Template.js
+cp ../out/bootstrap-scriptographer.js ~/Development/Scriptographer/Scriptographer/src/js/core/lib/00_bootstrap.js
+# cp ../out/Template-rhino.js ~/Development/Scriptographer/Scriptographer/src/js/core/lib/Template.js
 
 # doclet
-cp ../out/Template-rhino.js ~/Development/Scriptographer/Scriptographer/build/java/doclet/template.js
-cp ../out/bootstrap-rhino.js ~/Development/Scriptographer/Scriptographer/build/java/doclet/bootstrap.js
+cp ../out/Template-rhino.js ~/Development/Scriptographer/Scriptographer/src/js/doclet/lib/Template.js
+cp ../out/bootstrap-rhino.js ~/Development/Scriptographer/Scriptographer/src/js/doclet/lib/bootstrap.js
