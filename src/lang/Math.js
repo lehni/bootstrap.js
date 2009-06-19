b18a168d2845283a5122ccdbd6e2ebe51bca44ae
Jürg Lehni <juerg@scratchdisk.com>
2009-06-19 15:40:57 +0000

    Added one parameter version of Math.rand and use in in Array, also for the newly introduced Array#pick.
    git-svn-id: https://svn.scratchdisk.com/bootstrap/trunk@322 917dd4d6-6a9c-4700-b622-114467236220

--- src/lang/Math.js
+++ src/lang/Math.js
@@ -5,10 +5,13 @@
 // Math
 
 /**
- * Returns a random integer number min <= x < max
+ * Returns a random integer number first <= x < second, if two arguments are
+ * passed or 0 <= x < first, if there is donly one.
  */
-Math.rand = function(min, max) {
-	return Math.floor(Math.random() * (max - min) + min);
+Math.rand = function(first, second) {
+	return second == undefined
+		? Math.rand(0, first)
+		: Math.floor(Math.random() * (max - min) + min);
 }
 
 #endif // __lang_Math__
