9741e5de4d579462bc46a9230c35d7235cb8d7da
Jürg Lehni <juerg@scratchdisk.com>
2009-06-19 20:19:56 +0000

    Added static fields to Date to represent different types of timeframes in milliseconds.
    git-svn-id: https://svn.scratchdisk.com/bootstrap/trunk@326 917dd4d6-6a9c-4700-b622-114467236220

--- src/lang/Date.js
+++ src/lang/Date.js
@@ -7,6 +7,14 @@
 Date.inject({
 #ifndef RHINO // TODO: Consider BROWSER_LEGACY at a later point
 	statics: {
+		SECOND: 1000,
+		MINUTE: 60000,
+		HOUR: 3600000,
+		DAY: 86400000,
+		WEEK: 604800000, // 7 * DAY
+		MONTH: 2592000000, // 30 * DAY
+		YEAR: 31536000000, // 365 * DAY
+
 		now: Date.now || function() {
 			return +new Date();
 		}
