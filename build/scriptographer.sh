6b3ed4b490126131783f456f0bf1627f50b72047
Jürg Lehni <juerg@scratchdisk.com>
2009-07-21 01:13:38 +0000

    Fixed a bug in code wrongly ported from Mootools.
    git-svn-id: https://svn.scratchdisk.com/bootstrap/trunk@335 917dd4d6-6a9c-4700-b622-114467236220

--- build/scriptographer.sh
+++ build/scriptographer.sh
@@ -3,7 +3,7 @@
 # scriptographer
 cp ../out/bootstrap-scriptographer.js ~/Development/Scriptographer/Scriptographer/src/js/core/lib/00_bootstrap.js
 # cp ../out/Template-rhino.js ~/Development/Scriptographer/Scriptographer/src/js/core/lib/Template.js
-cp ../out/bootstrap-browser.js ~/Development/Scriptographer/Scriptographer/docs/js/resources/bootstrap.js
+cp ../out/bootstrap-browser.js ~/Development/Scriptographer/Scriptographer/docs/js/resources/js/bootstrap.js
 
 # doclet
 cp ../out/Template-rhino.js ~/Development/Scriptographer/Scriptographer/src/js/doclet/lib/Template.js
