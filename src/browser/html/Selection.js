65d525d4be60c8a2052e2886a57a5a498ca86147
Jürg Lehni <juerg@scratchdisk.com>
2009-10-09 12:11:15 +0000

    - Improved get/setSelection code, mostly based on Mootools. - Renamed get/setCaretPosition to get/setCaret.
    git-svn-id: https://svn.scratchdisk.com/bootstrap/trunk@353 917dd4d6-6a9c-4700-b622-114467236220

--- src/browser/html/Selection.js
+++ src/browser/html/Selection.js
@@ -10,56 +10,67 @@ FormElement.inject({
 	setSelection: function(start, end) {
 		var sel = end == undefined ? start : { start: start, end: end };
 		this.focus();
-		if(Browser.TRIDENT) {
+		if(this.$.setSelectionRange) {
+			this.$.setSelectionRange(sel.start, sel.end);
+		} else {
+			var value = this.getValue();
+			var len = value.substring(sel.start, sel.end).replace(/\r/g, '').length;
+			var pos = value.substring(0, sel.start).replace(/\r/g, '').length;
 			var range = this.$.createTextRange();
 			range.collapse(true);
-			range.moveStart('character', sel.start);
-			range.moveEnd('character', sel.end - sel.start);
+			range.moveEnd('character', pos + len);
+			range.moveStart('character', pos);
 			range.select();
-		} else this.$.setSelectionRange(sel.start, sel.end);
+		}
 		return this;
 	},
- 
+
 	getSelection: function() {
-		if(Browser.TRIDENT) {
+		if (this.$.selectionStart !== undefined) {
+			return { start: this.$.selectionStart, end: this.$.selectionEnd };
+		} else {
 			this.focus();
-			var range = document.selection.createRange();
-			var tmp = range.duplicate();
-			tmp.moveToElementText(this.$);
-			tmp.setEndPoint('EndToEnd', range);
-			return { start: tmp.text.length - range.text.length, end: tmp.text.length };
+			var pos = { start: 0, end: 0 };
+			var range = this.getDocument().$.selection.createRange();
+			var dup = range.duplicate();
+			if (this.$.type == 'text') {
+				pos.start = 0 - dup.moveStart('character', -100000);
+				pos.end = pos.start + range.text.length;
+			} else {
+				var value = this.getValue();
+				var offset = value.length - value.match(/[\n\r]*$/)[0].length;
+				dup.moveToElementText(this.$);
+				dup.setEndPoint('StartToEnd', range);
+				pos.end = offset - dup.text.length;
+				dup.setEndPoint('StartToStart', range);
+				pos.start = offset - dup.text.length;
+			}
+			return pos;
 		}
-		return { start: this.$.selectionStart, end: this.$.selectionEnd };
 	},
- 
+
 	getSelectedText: function() {
-/* needed? the other should allways produce the right result.
-		if(Browser.TRIDENT)
-			return document.selection.createRange().text;
-*/
  		var range = this.getSelection();
 		return this.getValue().substring(range.start, range.end);
 	},
 
 	replaceSelectedText: function(value, select) {
 		var range = this.getSelection(), current = this.getValue();
-		// Fix Firefox scroll bug, see http://userscripts.org/scripts/review/9452
+		// Fix Firefox scroll bug, see http://userscripts.org/scripts/review/9452, #insertAtCaret()
 		var top = this.$.scrollTop, height = this.$.scrollHeight;
 		this.setValue(current.substring(0, range.start) + value + current.substring(range.end, current.length));
 		if(top != null)
 			this.$.scrollTop = top + this.$.scrollHeight - height;
 		return select || select == undefined
 			? this.setSelection(range.start, range.start + value.length)
-			: this.setCaretPosition(range.start + value.length);
+			: this.setCaret(range.start + value.length);
 	},
 
-	getCaretPosition: function() {
+	getCaret: function() {
 		return this.getSelection().start;
 	},
- 
-	setCaretPosition: function(pos) {
-		if(pos == -1)
-			pos = this.getValue().length;
+
+	setCaret: function(pos) {
 		return this.setSelection(pos, pos);
 	}
 });
